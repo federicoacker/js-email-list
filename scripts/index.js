0c43c0003fef67c7675e9e1c4b2442f5990b21f3
federicoacker <acker.federico@gmail.com>
2026-04-15 14:16:23 +0200

    Fatto un tentativo con le async function, ma giusto per capire come funzionavano e come poter rimandare dei dati fuori da del codice asincrono

--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,12 @@ function createEmailEntry(number, name, email){
     </tr>
     `
 }
-
+let counter = 0;
 // Andiamo ora ad usare la fetchAPI, creando una funzione il cui compito è generarmi 10 email
-function fetchEmail(){
-    let counter = 0;
-    fetch(EMAIL_API_URL)
+async function fetchEmail(){
+    
+    let innerHTMLString = "";
+    innerHTMLString = await fetch(EMAIL_API_URL)
     .then((response) => response.json())
     .then((responseJson) => {
         const {success, response} = responseJson;
@@ -22,10 +23,15 @@ function fetchEmail(){
             const nameArray = name.split('');
             nameArray[0] = nameArray[0].toUpperCase();
             name = nameArray.join('');
-            const emailHTML = createEmailEntry(0, name, response);
-            emailListContainerEl.innerHTML += emailHTML;
+            
+            const emailHTML = createEmailEntry(counter, name, response);
+            counter++;
+            return emailHTML;
         }
     });
+    emailListContainerEl.innerHTML += await innerHTMLString;
 }
 
-fetchEmail();
+for(let i = 0; i<15; i++){
+    fetchEmail();
+}
