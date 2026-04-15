8c6d5e8a0cb1586c06e002d6440843079d8d7f46
federicoacker <acker.federico@gmail.com>
2026-04-15 14:23:18 +0200

    Trasformato la funzione fetchEmail in una funzione sincrona che semplicemente genera la lista facendo un ciclo for che itera 10 volte e aggiorna ogni volta l'innerHTML con una nuova email (un po' pesante come soluzione e la preferivo con la funzione asincrona)

--- scripts/index.js
+++ scripts/index.js
@@ -9,29 +9,24 @@ function createEmailEntry(number, name, email){
     </tr>
     `
 }
-let counter = 0;
 // Andiamo ora ad usare la fetchAPI, creando una funzione il cui compito è generarmi 10 email
-async function fetchEmail(){
-    
-    let innerHTMLString = "";
-    innerHTMLString = await fetch(EMAIL_API_URL)
-    .then((response) => response.json())
-    .then((responseJson) => {
-        const {success, response} = responseJson;
-        if(success){
-            let name = response.split("@")[0]; //Selezioniamo la prima parte della email, che sarà per me, il mio nome
-            const nameArray = name.split('');
-            nameArray[0] = nameArray[0].toUpperCase();
-            name = nameArray.join('');
-            
-            const emailHTML = createEmailEntry(counter, name, response);
-            counter++;
-            return emailHTML;
-        }
-    });
-    emailListContainerEl.innerHTML += await innerHTMLString;
+function generateEmailList(number){
+    for(let i = 0; i<number; i++){
+        fetch(EMAIL_API_URL)
+        .then((response) => response.json())
+        .then((responseJson) => {
+            const {success, response} = responseJson;
+            if(success){
+                let name = response.split("@")[0]; //Selezioniamo la prima parte della email, che sarà per me, il mio nome
+                const nameArray = name.split('');
+                nameArray[0] = nameArray[0].toUpperCase();
+                name = nameArray.join('');
+                const emailHTML = createEmailEntry(i, name, response);
+                emailListContainerEl.innerHTML += emailHTML;
+            }
+        });
+    }
+
 }
 
-for(let i = 0; i<15; i++){
-    fetchEmail();
-}
+generateEmailList(10);
