5031bb3bb0b1c62958104bb9d078eb6f98fa2a5c
federicoacker <acker.federico@gmail.com>
2026-04-15 14:06:33 +0200

    Creato funzione fetch la quale va a crearmi per ora, 1 sola entry come stringa

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const emailListContainerEl = document.querySelector("#email-list-container");
-
+const EMAIL_API_URL = "https://flynn.boolean.careers/exercises/api/random/mail";
 function createEmailEntry(number, name, email){
     return `
     <tr>
@@ -9,3 +9,23 @@ function createEmailEntry(number, name, email){
     </tr>
     `
 }
+
+// Andiamo ora ad usare la fetchAPI, creando una funzione il cui compito è generarmi 10 email
+function fetchEmail(){
+    let counter = 0;
+    fetch(EMAIL_API_URL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+        const {success, response} = responseJson;
+        if(success){
+            const name = response.split("@")[0]; //Selezioniamo la prima parte della email, che sarà per me, il mio nome
+            const nameArray = name.split('');
+            nameArray[0] = nameArray[0].toUpperCase;
+            name = nameArray.join('');
+            const emailHTML = createEmailEntry(0, name, response);
+            emailListContainerEl.innerHTML += emailHTML;
+        }
+    });
+}
+
+fetchEmail();
