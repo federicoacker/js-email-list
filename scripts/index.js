38fb2bf039d0820fbe3d79a76ca81f5b237f5f89
federicoacker <acker.federico@gmail.com>
2026-04-15 13:56:20 +0200

    Creata funzione che crea una emailEntry, crea solo la stringa html

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,11 @@
 const emailListContainerEl = document.querySelector("#email-list-container");
+
+function createEmailEntry(number, name, email){
+    return `
+    <tr>
+        <td>${number}</td>
+        <td>${name}</td>
+        <td>${email}</td>
+    </tr>
+    `
+}
