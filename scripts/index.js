e84c26e006024ecebb579cfe6acb3e6672a2cc21
federicoacker <acker.federico@gmail.com>
2026-04-16 10:13:17 +0200

    Aggiunto l'annotazione di tipo anche all'oggetto dom

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,10 @@
 const EMAIL_API_URL = "https://flynn.boolean.careers/exercises/api/random/mail";
 //VARIABILI
 
+/**
+ * @type {{emailListContainerEl:?HTMLElement, createList15Btn:?HTMLElement, createList20Btn:?HTMLElement, createList100Btn:?HTMLElement}}
+ */
+
 const dom = {
     emailListContainerEl : document.querySelector("#email-list-container"),
     createList15Btn : document.querySelector("#create-list-15"),
